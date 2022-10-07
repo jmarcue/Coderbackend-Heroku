2d1870f093e9eda8db13056f01fa9b8695a9f27b
Jaime Martinez <jmartinezcuevas@gmail.com>
2022-10-07 19:00:52 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -11,7 +11,15 @@ import { serverConfig } from './configs/server.config.js';
 import { __dirname, __dirJoin, numCPUs } from './utils/helper.util.js';
 
 // import  routes
+import productRoute from './routes/product.route.js';
+import messageRoute from './routes/message.route.js';
+import userRoute from './routes/user.route.js';
 import infoRoute from './routes/info.route.js';
+import randomRoute from './routes/random.route.js';
+
+// import controllers
+import messageClass from './controllers/message.controller.js';
+import productClass from './controllers/product.controller.js';
 
 // Server 
 const app = express();
@@ -50,8 +58,32 @@ app.set('views', __dirJoin(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 // endpoints
+app.use('/api/productos', productRoute);
+app.use('/randoms', randomRoute);
+app.use('/mensajes', messageRoute);
+app.use('/user', userRoute);
 app.use('/info', infoRoute);
 app.get('/', function (req, res) { res.render('index') });
+
+// Sockets
+let toChat = []
+io.on('connection', socket => {
+  console.log(`Cliente hash:${socket.id} inició conexión`);
+  io.sockets.emit('new-message-server', toChat)
+
+  socket.on('new-message', async data => {
+      const message = await data;
+      toChat.push(data);
+      messages.addMsg({ message });
+      io.sockets.emit('new-message-server', toChat);
+  });
+
+  socket.on('new-producto', async data => {
+      const producto = await data;
+      prodClass.add({ producto });
+      io.sockets.emit('new-prod-server', producto);
+  });
+});
 
 // server connection
 const server = http.listen(PORT, () => {
